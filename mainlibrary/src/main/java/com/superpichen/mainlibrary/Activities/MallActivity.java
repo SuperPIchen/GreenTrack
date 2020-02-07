c81b46de3316dc04cc3e4ff5d33c4b3bc01d2ae4
ibo19 <ibo1909053269@163.com>
2020-02-08 00:05:38 +0800

    去mac前的备份

--- mainlibrary/src/main/java/com/superpichen/mainlibrary/Activities/MallActivity.java
+++ mainlibrary/src/main/java/com/superpichen/mainlibrary/Activities/MallActivity.java
@@ -3,7 +3,9 @@ package com.superpichen.mainlibrary.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.superpichen.mainlibrary.MyView.TopBar.StatusBarUtil;
 import com.superpichen.mainlibrary.R;
@@ -19,5 +21,10 @@ public class MallActivity extends AppCompatActivity {
         StatusBarUtil.setTranslucentForImageView(this,0,null);
         intent=getIntent();
         setResult(FinalValue.GETPOINTFORMALLACTIVITY);
+        //状态栏中的文字颜色和图标颜色，需要android系统6.0以上，而且目前只有一种可以修改（一种是深色，一种是浅色即白色）
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //修改为深色，因为我们把状态栏的背景色修改为主题色白色，默认的文字及图标颜色为白色，导致看不到了。
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 }
