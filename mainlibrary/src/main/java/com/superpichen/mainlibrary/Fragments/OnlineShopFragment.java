84e30e1311393bb6d941e00d09f54275cf85110e
superpichen <ibo1909053269@163.com>
2020-02-09 19:05:59 +0800

    第二次上交内容

--- mainlibrary/src/main/java/com/superpichen/mainlibrary/Fragments/OnlineShopFragment.java
+++ mainlibrary/src/main/java/com/superpichen/mainlibrary/Fragments/OnlineShopFragment.java
@@ -20,6 +20,7 @@ import com.stx.xhb.androidx.transformers.Transformer;
 import com.superpichen.mainlibrary.R;
 import com.superpichen.mainlibrary.Tools.JavaTools.BaseFragment;
 import com.superpichen.mainlibrary.Tools.JavaTools.FraOnlineXbanner1Info;
+import com.superpichen.mainlibrary.Tools.JavaTools.FraOnlineXbanner2Info;
 import com.superpichen.mainlibrary.Tools.JavaTools.ScreenUtil;
 
 import java.util.ArrayList;
@@ -32,6 +33,7 @@ public class OnlineShopFragment extends BaseFragment {
     private ImageView IvFraOnlineChange;
     private NiceImageView NvFraOnlineUser;
     private XBanner XbFraOnlineFirst;
+    private XBanner XbFraOnlineSecond;
 
     /**
      * Find the Views in the layout<br />
@@ -43,15 +45,14 @@ public class OnlineShopFragment extends BaseFragment {
         IvFraOnlineChange = (ImageView)view.findViewById( R.id.IvFraOnlineChange );
         NvFraOnlineUser = (NiceImageView)view.findViewById( R.id.NvFraOnlineUser );
         XbFraOnlineFirst = (XBanner)view.findViewById( R.id.XbFraOnlineFirst );
+        XbFraOnlineSecond = view.findViewById(R.id.XbFraOnlineSecond);
     }
 
 
-    private View view;
     public OnlineShopFragment() {
         // Required empty public constructor
     }
 
-    private List<FraOnlineXbanner1Info> data1=new ArrayList<>();
     @Override
     protected View initView() {
         View view=View.inflate(getContext(),R.layout.fragment_online_shop,null);
@@ -62,8 +63,73 @@ public class OnlineShopFragment extends BaseFragment {
     @Override
     protected void initData() {
         super.initData();
+        setBanner1();
+        setBanner2();
+    }
+
+    /**
+     * 设置第二条Banner
+     */
+    private void setBanner2() {
+        List<FraOnlineXbanner2Info> data2=new ArrayList<>();
+        data2.add(new FraOnlineXbanner2Info("绿萝-附有灵气的植物","让家园水灵灵",1.2,R.drawable.fraonlinebanner2_1,
+                "樱桃蛋糕-体力恢复×2","款待你的碳宠物",2,R.drawable.fraonlinebanner2_2,
+                "哈士奇-顽皮的碳宠物","让你的家园充满活力",19.9,R.drawable.fraonlinebanner2_3));
+        data2.add(new FraOnlineXbanner2Info("甜甜圈-小心宠物蛀牙","甜蜜的味道",1.5,R.drawable.fraonlinebanner2_4,
+                "小野花-庄园中的一抹红","点缀你的家园",3,R.drawable.fraonlinebanner2_5,
+                "小白猫-冷库的温暖","不小心酷到你",29.9,R.drawable.fraonlinebanner2_6));
+        data2.add(new FraOnlineXbanner2Info("仙人掌-不用太操心他","顽强的植物",1.9,R.drawable.fraonlinebanner2_7,
+                "小恐龙-绿油油的\"猛兽\"","可爱的侏罗纪动物",49.9,R.drawable.fraonlinebanner2_8,
+                "冰淇淋-酷暑一夏","最好的甜品",1.8,R.drawable.fraonlinebanner2_9));
+        XbFraOnlineSecond.setPageTransformer(Transformer.Default);
+        XbFraOnlineSecond.setBannerData(R.layout.item_fraonline_xbanner_second,data2);
+        XbFraOnlineSecond.loadImage(new XBanner.XBannerAdapter() {
+            @Override
+            public void loadBanner(XBanner banner, Object model, View view, int position) {
+                NiceImageView NvItemFraOnline1= view.findViewById( R.id.NvItemFraOnline1 );
+                TextView TvItemFraOnlineBanner2Title1= view.findViewById( R.id.TvItemFraOnlineBanner2Title1 );
+                TextView TvItemFraOnlineBanner2Tag1= view.findViewById( R.id.TvItemFraOnlineBanner2Tag1 );
+                TextView TvFraOnlineBanner2Money1= view.findViewById( R.id.TvFraOnlineBanner2Money1 );
+                NiceImageView NvItemFraOnline2= view.findViewById( R.id.NvItemFraOnline2 );;
+                TextView TvItemFraOnlineBanner2Title2= view.findViewById( R.id.TvItemFraOnlineBanner2Title2 );;
+                TextView TvItemFraOnlineBanner2Tag2= view.findViewById( R.id.TvItemFraOnlineBanner2Tag2 );
+                TextView TvFraOnlineBanner2Money2= view.findViewById( R.id.TvFraOnlineBanner2Money2 );
+                NiceImageView NvItemFraOnline3= view.findViewById( R.id.NvItemFraOnline3 );
+                TextView TvItemFraOnlineBanner2Title3= view.findViewById( R.id.TvItemFraOnlineBanner2Title3 );
+                TextView TvItemFraOnlineBanner2Tag3= view.findViewById( R.id.TvItemFraOnlineBanner2Tag3 );
+                TextView TvFraOnlineBanner2Money3= view.findViewById( R.id.TvFraOnlineBanner2Money3 );
+                NvItemFraOnline1.setImageResource(((FraOnlineXbanner2Info)model).getImg1());
+                TvItemFraOnlineBanner2Title1.setText(((FraOnlineXbanner2Info)model).getTitle1());
+                TvItemFraOnlineBanner2Tag1.setText(((FraOnlineXbanner2Info)model).getTag1());
+                TvFraOnlineBanner2Money1.setText(((FraOnlineXbanner2Info)model).getMoney1()+"币");
+                NvItemFraOnline2.setImageResource(((FraOnlineXbanner2Info)model).getImg2());
+                TvItemFraOnlineBanner2Title2.setText(((FraOnlineXbanner2Info)model).getTitle2());
+                TvItemFraOnlineBanner2Tag2.setText(((FraOnlineXbanner2Info)model).getTag2());
+                TvFraOnlineBanner2Money2.setText(((FraOnlineXbanner2Info)model).getMoney2()+"币");
+                NvItemFraOnline3.setImageResource(((FraOnlineXbanner2Info)model).getImg3());
+                TvItemFraOnlineBanner2Title3.setText(((FraOnlineXbanner2Info)model).getTitle3());
+                TvItemFraOnlineBanner2Tag3.setText(((FraOnlineXbanner2Info)model).getTag3());
+                TvFraOnlineBanner2Money3.setText(((FraOnlineXbanner2Info)model).getMoney3()+"币");
+
+            }
+        });
+        XbFraOnlineSecond.setOnItemClickListener(new XBanner.OnItemClickListener() {
+            @Override
+            public void onItemClick(XBanner banner, Object model, View view, int position) {
+
+            }
+        });
+    }
+
+    /**
+     * 设置第一条Banner
+     */
+    private void setBanner1() {
+        List<FraOnlineXbanner1Info> data1=new ArrayList<>();
         data1.add(new FraOnlineXbanner1Info("限时特惠","哆啦A梦地铁卡：带你梦回童年","每周免费乘车一次",R.drawable.fraonlinebanner1));
         data1.add(new FraOnlineXbanner1Info("宠物上新","碳宠物需要你的照顾","陪伴您的旅程",R.drawable.fraonlinebanner2));
+        data1.add(new FraOnlineXbanner1Info("生活用品","更多用品等你探索","简约而不简单",R.drawable.fraonlinebanner3));
+        data1.add(new FraOnlineXbanner1Info("帮你省钱","Green鲜果店代金券","维C你的生活",R.drawable.fraonlinebanner4));
         XbFraOnlineFirst.setPageTransformer(Transformer.Default);
         XbFraOnlineFirst.setBannerData(R.layout.item_fraonline_xbanner_first,data1);
         XbFraOnlineFirst.loadImage(new XBanner.XBannerAdapter() {
---
+++ mainlibrary/src/main/java/com/superpichen/mainlibrary/Tools/JavaTools/FraOnlineXbanner2Info.java
@@ -0,0 +1,135 @@
+package com.superpichen.mainlibrary.Tools.JavaTools;
+
+public class FraOnlineXbanner2Info extends com.stx.xhb.androidx.entity.SimpleBannerInfo{
+    private String title1;
+    private String tag1;
+    private double money1;
+    private int img1;
+    private String title2;
+    private String tag2;
+    private double money2;
+    private int img2;
+    private String title3;
+    private String tag3;
+    private double money3;
+    private int img3;
+
+    public FraOnlineXbanner2Info() {
+    }
+
+    public FraOnlineXbanner2Info(String title1, String tag1, double money1, int img1, String title2, String tag2, double money2, int img2, String title3, String tag3, double money3, int img3) {
+        this.title1 = title1;
+        this.tag1 = tag1;
+        this.money1 = money1;
+        this.img1 = img1;
+        this.title2 = title2;
+        this.tag2 = tag2;
+        this.money2 = money2;
+        this.img2 = img2;
+        this.title3 = title3;
+        this.tag3 = tag3;
+        this.money3 = money3;
+        this.img3 = img3;
+    }
+
+    public String getTitle1() {
+        return title1;
+    }
+
+    public void setTitle1(String title1) {
+        this.title1 = title1;
+    }
+
+    public String getTag1() {
+        return tag1;
+    }
+
+    public void setTag1(String tag1) {
+        this.tag1 = tag1;
+    }
+
+    public double getMoney1() {
+        return money1;
+    }
+
+    public void setMoney1(double money1) {
+        this.money1 = money1;
+    }
+
+    public int getImg1() {
+        return img1;
+    }
+
+    public void setImg1(int img1) {
+        this.img1 = img1;
+    }
+
+    public String getTitle2() {
+        return title2;
+    }
+
+    public void setTitle2(String title2) {
+        this.title2 = title2;
+    }
+
+    public String getTag2() {
+        return tag2;
+    }
+
+    public void setTag2(String tag2) {
+        this.tag2 = tag2;
+    }
+
+    public double getMoney2() {
+        return money2;
+    }
+
+    public void setMoney2(double money2) {
+        this.money2 = money2;
+    }
+
+    public int getImg2() {
+        return img2;
+    }
+
+    public void setImg2(int img2) {
+        this.img2 = img2;
+    }
+
+    public String getTitle3() {
+        return title3;
+    }
+
+    public void setTitle3(String title3) {
+        this.title3 = title3;
+    }
+
+    public String getTag3() {
+        return tag3;
+    }
+
+    public void setTag3(String tag3) {
+        this.tag3 = tag3;
+    }
+
+    public double getMoney3() {
+        return money3;
+    }
+
+    public void setMoney3(double money3) {
+        this.money3 = money3;
+    }
+
+    public int getImg3() {
+        return img3;
+    }
+
+    public void setImg3(int img3) {
+        this.img3 = img3;
+    }
+
+    @Override
+    public Object getXBannerUrl() {
+        return null;
+    }
+}
